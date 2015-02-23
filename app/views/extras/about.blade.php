c33eb8f5edbed2a553527f31e6cd059f9d860888
Marissa Vega <marissavega90@gmail.com>
2015-02-23 11:54:14 -0600

    Finished About Us page and products page

--- app/views/extras/about.blade.php
+++ app/views/extras/about.blade.php
@@ -3,22 +3,35 @@
 
 <div class="container">
 	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-10 col-md-offset-1">
-					<img src="/assets/img/ChrisF.jpg" class="picture-chris">
-						<div class="col-md-5">
-						<h1>Chris Fuhrman</h1>
-						<h3 class="">Chris was the idea behind InsureJoy.</h3>
-						</div>
-				</div>		
-			</div>
-			<img src="/assets/img/TravisF.jpg" class="picture-travis">
-				<p>About Travis</p>
-			
-			<img src="/assets/img/MarissaV.jpg" class="picture-marissa">
-				<p>About Marissa</p>
-		</div>
+	  <div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/ChrisF.jpg" alt="..." class="picture-chris">
+	      <div class="caption">
+	        <h3>Chris Fuhrman</h3>
+	        <p>Chris has a strong background in business operations and enjoyed his work over the past 5 years.  However, he was drawn to the tech field for a variety of reasons and wanted to be able to create and build things - not just work at a tech company doing operations.  This led him to move from Philadelphia to San Antonio to attend Codeup's coding bootcamp.
+
+			Chris thrived in Codeup's learning environment and can now function as a junior level programmer.  Chris is eager to start putting his skill to work. </p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/TravisF.jpg" alt="..." class="picture-travis">
+	      <div class="caption">
+	        <h3>Travis French</h3>
+	        <p>In this modern age where the internet and software has saturated every aspect of our lives, I felt that I was lacking. Even though I have a background in banking and customer service, this did not satisfy my creative desire. When I heard about Codeup I jumpted on the opportunity to expand my potential because of how fast this industry is growing</p>
+	      </div>
+	    </div>
+	  </div>
+	<div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/MarissaV.jpg" alt="..." class="picture-marissa">
+	      <div class="caption">
+	        <h3>Marissa Vega</h3>
+	        <p>I used to be a creative writing major before I learned about Codeup. I've always been interested in the arts of all kinds, I love literature and have taken many art, writing, and music classes. All of this has given me a better perspective on designing interfaces and working on making both front-end and back-end design harmonious for users. I love beautiful design and have used my beginner's skills to help make this website powerful and wonderful to look at, and I hope to have many more opportunities in the future to work with users first-hand in creating fantastic user experiences.</p>
+	      </div>
+	    </div>
+	  </div>
 	</div>
 </div>
 
--- app/views/extras/products.blade.php
+++ app/views/extras/products.blade.php
@@ -1,12 +1,80 @@
 @extends('layouts.master')
 @section('content')
 
-<div class="container">
-	<div class="row">
-		<div class="col-md-6 col-md-offset-3">
-			<img src="/assets/img/insurance_logos.gif">
-		</div>
+
+
+<div class="row col-md-10 col-md-offset-4 margin-top bottom-margin">
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+	 
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner" role="listbox">
+	    <div class="item active">
+	    	<div class="row col-md-6">
+			      <img src="/assets/img/aig_logo.jpg" class="aig">
+			</div>
+	      
+	    </div>
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/allied_logo.jpg" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/allstate_logo.jpg" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/farmers_logo.jpg" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/hartford-logo.png" class="allied">
+	      	</div>
+	     
+	    </div>
+	   
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/ING-logo.png" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/liberty_logo.jpg" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/metlife_logo.png" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	    <div class="item">
+	    	<div class="row col-md-6">
+	      		<img src="/assets/img/pacific_logo.jpg" class="allied">
+	      	</div>
+	     
+	    </div>
+
+	  </div>
+
+	  <!-- Controls -->
+	  
 	</div>
 </div>
-
 @stop
