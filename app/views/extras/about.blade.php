d6c79f02375572fa7f20da95d0b0df645a91b4e5
Marissa Vega <marissavega90@gmail.com>
2015-02-23 12:59:16 -0600

    Added about page

--- app/views/extras/about.blade.php
+++ app/views/extras/about.blade.php
@@ -3,6 +3,7 @@
 
 <div class="container">
 	<div class="row">
+<<<<<<< HEAD
 	  <div class="col-sm-4 col-md-4">
 	    <div class="thumbnail margin-top">
 	      <img src="/assets/img/ChrisF.jpg" alt="..." class="picture-chris">
@@ -28,10 +29,29 @@
 	      <img src="/assets/img/MarissaV.jpg" alt="..." class="picture-marissa">
 	      <div class="caption">
 	        <h3>Marissa Vega</h3>
-	        <p>I used to be a creative writing major before I learned about Codeup. I've always been interested in the arts of all kinds, I love literature and have taken many art, writing, and music classes. All of this has given me a better perspective on designing interfaces and working on making both front-end and back-end design harmonious for users. I love beautiful design and have used my beginner's skills to help make this website powerful and wonderful to look at, and I hope to have many more opportunities in the future to work with users first-hand in creating fantastic user experiences.</p>
+	        <p>I used to be a creative writing major before I learned about Codeup. I've always been interested in the arts of all kinds, I love literature and have taken many art, writing, and music classes. All of this has given me a better perspective on designing interfaces and working on making both front-end and back-end design harmonious for users. I love beautiful design and have used my beginner's skills to help make this website powerful and wonderful to look at, and I hope to have many more opportunities in the future to work with users first-hand in creating fantastic user experiences.l
+	        </p>
 	      </div>
 	    </div>
 	  </div>
+=======
+		<div class="col-md-12">
+			<div class="row">
+				<div class="col-md-10 col-md-offset-1">
+					<img src="/assets/img/ChrisF.jpg" class="picture-chris">
+						<div class="col-md-5">
+						<h1>Chris Fuhrman</h1>
+						<h3 class="">Chris was the idea behind InsureJoy.</h3>
+						</div>
+				</div>		
+			</div>
+			<img src="/assets/img/TravisF.jpg" class="picture-travis">
+				<p>About Travis</p>
+			
+			<img src="/assets/img/MarissaV.jpg" class="picture-marissa">
+				<p>About Marissa</p>
+		</div>
+>>>>>>> d6c111184f4c70ed3b250545dbe4422313362fae
 	</div>
 </div>
 
