d855233c65ad6f2dfa6dac166805ab75729df613
Marissa Vega <marissavega90@gmail.com>
2015-02-23 12:54:18 -0600

    About page

---
+++ app/views/extras/about.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.master')
+@section('content')
+
+<div class="container">
+	<div class="row">
+	  <div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/ChrisF.jpg" alt="..." class="picture-chris">
+	      <div class="caption">
+	        <h3>Chris Fuhrman</h3>
+	        <p>Chris has a strong background in business operations and enjoyed his work over the past 5 years.  However, he was drawn to the tech field for a variety of reasons and wanted to be able to create and build things - not just work at a tech company doing operations.  This led him to move from Philadelphia to San Antonio to attend Codeup's coding bootcamp.
+
+			Chris thrived in Codeup's learning environment and can now function as a junior level programmer.  Chris is eager to start putting his skill to work. </p>
+	      </div>
+	    </div>
+	  </div>
+	  <div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/TravisF.jpg" alt="..." class="picture-travis">
+	      <div class="caption">
+	        <h3>Travis French</h3>
+	        <p>In this modern age where the internet and software has saturated every aspect of our lives, I felt that I was lacking. Even though I have a background in banking and customer service, this did not satisfy my creative desire. When I heard about Codeup I jumpted on the opportunity to expand my potential because of how fast this industry is growing</p>
+	      </div>
+	    </div>
+	  </div>
+	<div class="col-sm-4 col-md-4">
+	    <div class="thumbnail margin-top">
+	      <img src="/assets/img/MarissaV.jpg" alt="..." class="picture-marissa">
+	      <div class="caption">
+	        <h3>Marissa Vega</h3>
+	        <p>I used to be a creative writing major before I learned about Codeup. I've always been interested in the arts of all kinds, I love literature and have taken many art, writing, and music classes. All of this has given me a better perspective on designing interfaces and working on making both front-end and back-end design harmonious for users. I love beautiful design and have used my beginner's skills to help make this website powerful and wonderful to look at, and I hope to have many more opportunities in the future to work with users first-hand in creating fantastic user experiences.l
+	        </p>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+</div>
+
+
+@stop
