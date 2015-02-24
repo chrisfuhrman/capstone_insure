341cefb170597789d9e9fdaef80e0d5db6258f43
Chris Fuhrman <chrisfuhrman@gmail.com>
2015-02-24 12:05:43 -0600

    fixed nullable syntax

--- app/database/migrations/2015_02_09_142728_create_users_table.php
+++ app/database/migrations/2015_02_09_142728_create_users_table.php
@@ -19,21 +19,21 @@ class CreateUsersTable extends Migration {
                   $table->rememberToken();
 
                   $table->string('email')->unique();
-                  $table->string('first_name')->nullable;
-                  $table->string('middle_name')->nullable;
-                  $table->string('last_name')->nullable;
-                  $table->string('maiden_name')->nullable;
-                  $table->date('dob')->nullable;
-                  $table->string('gender', 1)->nullable;
+                  $table->string('first_name')->nullable();
+                  $table->string('middle_name')->nullable();
+                  $table->string('last_name')->nullable();
+                  $table->string('maiden_name')->nullable();
+                  $table->date('dob')->nullable();
+                  $table->string('gender', 1)->nullable();
 
-                  $table->string('home_phone')->nullable;
-                  $table->string('work_phone')->nullable;
+                  $table->string('home_phone')->nullable();
+                  $table->string('work_phone')->nullable();
                   
-                  $table->string('address')->nullable;
-                  $table->string('address2')->nullable;
-                  $table->string('city')->nullable;
-                  $table->string('state')->nullable;
-                  $table->string('zip')->nullable;
+                  $table->string('address')->nullable();
+                  $table->string('address2')->nullable();
+                  $table->string('city')->nullable();
+                  $table->string('state')->nullable();
+                  $table->string('zip')->nullable();
 
                   $table->timestamps();
             });
